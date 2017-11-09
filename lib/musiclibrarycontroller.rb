6c0cf42c37e7d8715eb9e10a29f46d6d759eb8d5
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-09 22:07:26 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -77,12 +77,12 @@ class MusicLibraryController
     song_to_play = gets.chomp
 
     binding.pry
-    
+
       # if song_to_play.to_i >= 1 && song_to_play.to_i <= song_list.length
       #   "Playing #{song.name} by #{song.artist.name}"
       # end
 
-      
+
   end
 
 end
